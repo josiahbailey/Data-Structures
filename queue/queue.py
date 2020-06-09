25cf34a78e95b302adae44aa902a26d8420ed11f
Josiah <reikiryo@yahoo.com>
2020-06-09 16:06:12 -0400

    queue using array finished

--- queue/queue.py
+++ queue/queue.py
@@ -19,23 +19,42 @@ Stretch: What if you could only use instances of your Stack class to implement t
 # path.append('../singly_linked_list')
 # from singly_linked_list import LinkedList
 
-from singly_linked_list.singly_linked_list import LinkedList
+# from singly_linked_list.singly_linked_list import LinkedList
+
+
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = LinkedList()
+
+#     def __len__(self):
+#         return self.size
+
+#     def enqueue(self, value):
+#         self.size += 1
+#         self.storage.add_to_tail(value)
+
+#     def dequeue(self):
+#         if self.size == 0:
+#             return None
+#         self.size -= 1
+#         return self.storage.remove_head()
 
 
 class Queue:
     def __init__(self):
         self.size = 0
-        self.storage = LinkedList()
+        self.storage = []
 
     def __len__(self):
         return self.size
 
     def enqueue(self, value):
         self.size += 1
-        self.storage.add_to_tail(value)
+        self.storage.append(value)
 
     def dequeue(self):
         if self.size == 0:
             return None
         self.size -= 1
-        return self.storage.remove_head()
+        return self.storage.pop(0)
